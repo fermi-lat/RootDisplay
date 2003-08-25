d70aebeffcf9e677a9f9f907963d6bdf4b407638
heather <>
2003-08-25 18:43:31 +0000

    initial import of new RootDisplay that adds Root specific menus to the GUI

---
+++ src/RootDisplay_dll.cxx
@@ -0,0 +1,9 @@
+/** @file RootDisplay_dll.cpp
+    @brief DLL entry point code, filled by macro.
+  $Header$
+*/
+
+#include "GaudiKernel/LoadFactoryEntries.h"
+LOAD_FACTORY_ENTRIES(RootDisplay)
+
+
---
+++ src/mainpage.h
@@ -0,0 +1,17 @@
+//$Header$
+// (Special "header" just for doxygen)
+
+/*! @mainpage  package RootDisplay
+
+@author H. Kelly
+
+  TODO: fill this in.
+
+  <hr>
+  \section jobOptions src/jobOptions.txt
+  \include jobOptions.txt
+  \section requirements requirements
+  \include requirements
+
+
+*/
