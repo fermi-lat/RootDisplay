a156112d1851197429ed88fa6815ac5d60823a30
ecephas <>
2008-12-09 00:08:36 +0000

    Check in RootDisplayLib.py for RootDisplay v0r3 (branch-tag, GlastReleasev15r49).

--- RootDisplayLib.py
+++ RootDisplayLib.py
@@ -1,7 +1,6 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/RootDisplay/RootDisplayLib.py,v 1.1 2008/08/15 21:42:36 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['RootDisplay'])
-
 def exists(env):
     return 1;
