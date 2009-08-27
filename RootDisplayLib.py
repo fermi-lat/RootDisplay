95e7e6ecd229367855f5fe4b287fc27c800df7b0
jrb <>
2009-08-27 18:09:21 +0000

    SConscript:  convert to registerTargets RootDisplayLib.py:  add some dependencies .cvsignore:  ignore build, *.pyc

--- RootDisplayLib.py
+++ RootDisplayLib.py
@@ -1,6 +1,8 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/RootDisplay/RootDisplayLib.py,v 1.1 2008/08/15 21:42:36 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/RootDisplay/RootDisplayLib.py,v 1.3 2009/01/23 00:21:02 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['RootDisplay'])
+    env.Tool('guiLib')
+    env.Tool('addLibrary', library = env['gaudiLibs'])
 def exists(env):
     return 1;
