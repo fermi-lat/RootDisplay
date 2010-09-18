c68d5983d2947b548bd8489334cc36c65821c423
heather <>
2010-09-18 03:52:12 +0000

    Gaudi v21r7 udpates

--- src/RootDisplay_dll.cxx
+++
@@ -1,9 +0,0 @@
-/** @file RootDisplay_dll.cpp
-    @brief DLL entry point code, filled by macro.
-  $Header$
-*/
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-LOAD_FACTORY_ENTRIES(RootDisplay)
-
-
